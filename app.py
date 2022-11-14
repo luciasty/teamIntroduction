accfcc9b0045ff7b48dfcf21a5066a2a78e23aaa
Park Mu Hyun <pla2697@gmail.com>
2022-11-14 21:11:50 +0900

    app.py 수정 static/script.js 수정 templates/index.html 수정

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://test:sparta@cluster0.ai4u91k.mongodb.net/?retryWrites=true&w=majority")
+db = client.dbsparta
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/creat-member", methods=["POST"])
+def creat_member():
+    image_receive = request.form["image_give"]
+    name_receive = request.form["name_give"]
+    yourself_receive = request.form["yourself_give"]
+    strong_receive = request.form["strong_give"]
+    goals_receive = request.form["goals_give"]
+    appointment_receive = request.form["appointment_give"]
+    sns_receive = request.form["sns_give"]
+
+
+    doc = {
+        "image": image_receive,
+        "name": name_receive,
+        "yourself": yourself_receive,
+        "strong": strong_receive,
+        "goals": goals_receive,
+        "appointment": appointment_receive,
+        "sns": sns_receive
+    }
+
+    db.members.insert_one(doc)
+    return jsonify({"msg": "추가 완료!"})
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
--- static/script.js
+++ static/script.js
@@ -5,3 +5,25 @@ function open_box() {
 function close_box() {
     $('#post-box').hide()
 }
+
+$(document).ready(function () {
+    $(".save_button").click(function () {
+        const image = $("#my_image").val();
+        const name = $("#my_name").val();
+        const yourself = $("#my_yourself").val();
+        const strong = $("#my_strong").val();
+        const goals = $("#my_goals").val();
+        const appointment = $("#my_appointment").val();
+        const sns = $("#my_sns").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/creat-member",
+            data: {image_give: image, name_give: name, yourself_give: yourself, strong_give: strong, goals_give: goals, appointment_give: appointment, sns_give: sns},
+            success: function (response) {
+                alert(response["msg"]);
+            }
+        });
+
+    });
+});
