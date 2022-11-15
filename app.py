445633825be02c586a0e8e760898ca1082430efe
Park Mu Hyun <pla2697@gmail.com>
2022-11-15 14:45:48 +0900

    profile.html profile.js profile.css 수정 및 추가

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ client = MongoClient("mongodb+srv://test:sparta@cluster0.ai4u91k.mongodb.net/?re
 db = client.dbsparta
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-
     return render_template("index.html")
+
+
+# 프로필 페이지로 이동하면 profile.html을 보여준다.
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
 
 # 팀원 추가
 # 숫자로 id를 주기 위해서 db에서 받아올 정보가 있기 때문에 GET을 사용한다
@@ -20,12 +26,10 @@ def creat_member():
     # 문서가 비었으면 id를 초기화 한다
     if (db.members.count_documents({}) == 0):
         id = 0
-    
     # 문서가 있다면 제일 최근 작성한 문서의 id를 받아온다.
     else:
         id = db.members.find().sort("_id", -1)[0]["id"]
-    
-    
+
     # 입력받은 값들을 받아서 변수 저장
     image_receive = request.form["image_give"]
     name_receive = request.form["name_give"]
@@ -38,7 +42,6 @@ def creat_member():
     # id가 겹치지 않게 1씩 더해서 id값을 증가시킨다.
     id += 1
 
-    
     # 받아온 값들을 문서로 만든다.
     doc = {
         "id": id,
@@ -51,12 +54,38 @@ def creat_member():
         "appointment": appointment_receive,
         "sns": sns_receive
     }
-    
+
     # 작성된 문서를 데이터베이스에 밀어넣는다
     db.members.insert_one(doc)
 
     # 완료된 메세지를 보낸다
     return jsonify({"msg": "추가 완료!"})
+
+
+# S.A에서 생각했던 API 주소대로 /members GET
+@app.route("/members", methods=["GET"])
+def members_get():
+    # 데이터베이스에서 _id를 제외한 모든 데이터를 가지고와서 리스트로 만들어 members에 저장
+    merbers = list(db.members.find({}, {"_id": False}))
+
+    # 리턴으로 저장한 members를 넘긴다.
+    return jsonify({"members": merbers})
+
+
+# 프로필 페이지가 로딩되면 팀원의 정보를 가져온다.
+@app.route("/profile-get", methods=["GET"])
+def profile_get():
+    # ajax에서 url에 직접 보내준 id_give파라미터를 가져온다.
+    id_receive = request.args.get('id_give')
+
+    # 가져온 파라미터 값이 String으로 가져와진다 그렇기 때문에 데이터베이스 id와 같은 자료형으로 변환 시켜준다.
+    id = int(id_receive)
+
+    # 데이터베이스에서 _id를 제외한 id의 팀원 정보를 검색해서 가져온다.
+    merber = db.members.find_one({"id": id}, {"_id": False})
+
+    # 리턴으로 저장한 member를 넘긴다.
+    return jsonify({"member": merber})
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -9,6 +9,60 @@ function close_box() {
 
 //모든 HTML 문서가 준비가 되었으면 함수를 실행한다.
 $(document).ready(function () {
+    // index페이지에 팀원 리스트 가져오기
+    $.ajax({
+        type: "get",
+        url: "/members",
+        data: {},
+        success: function (response) {
+            // 가져온 response안에 members리스트를 변수에 저장
+            // for of를 이용해 리스트를 반복
+            const members = response["members"];
+            for (const member of members) {
+                id = member["id"];
+                image = member["image"];
+                name = member["name"];
+                sns = member["sns"];
+
+                // html구조를 작성하고 가져온 데이터를 삽입한다.
+                temp_html = `<div class="card mb-3" style="max-width: 540px;">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="${image}"
+                                             class="img-fluid rounded-start" alt="...">
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card-body">
+                                            <h5 class="card-title title_color">${name}</h5>
+                                            <p class="card-text temp">서울: 18도</p>
+                                            <a href="${sns}" class="card-text">개발일지 블로그</a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card-body">
+                                            <button><a href="/profile?id=${id}">프로필 보기</a></button>
+                                            <p class="card-text">조회수: 10</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`;
+
+                // class members를 가지고 있는 요소에다가 작성해둔 temp_html을 추가한다.
+                $(".members").append(temp_html);
+
+            }
+        }
+    });
+    //스파르타 코딩클럽에서 제공하는 api에서 지역 온도를 가져온다.
+    $.ajax({
+        type: "get",
+        url: "http://spartacodingclub.shop/sparta_api/weather/seoul",
+        data: {},
+        success: function (response) {
+            $(".temp").text(response["temp"]);
+        }
+    });
+
     //팀원의 정보를 입력하고 버튼을 클릭하면 함수가 실행한다.
     $(".save_button").click(function () {
         //제이쿼리를 이용해 입력한 정보의 value를 가져온다.
@@ -27,7 +81,16 @@ $(document).ready(function () {
             //data를 모두 보내고 성공했으면 창을 새로고침 합니다.
             type: "POST",
             url: "/creat-member",
-            data: {image_give: image, name_give: name, yourself_give: yourself, strong_give: strong, style_give: style, goals_give: goals, appointment_give: appointment, sns_give: sns},
+            data: {
+                image_give: image,
+                name_give: name,
+                yourself_give: yourself,
+                strong_give: strong,
+                style_give: style,
+                goals_give: goals,
+                appointment_give: appointment,
+                sns_give: sns
+            },
             success: function (response) {
                 alert(response["msg"]);
                 window.location.reload();
