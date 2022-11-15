c0b983d61d964be249ef6f8930a38129ac4db8c7
Park Mu Hyun <pla2697@gmail.com>
2022-11-15 11:13:39 +0900

    app.py 수정 static/script.js 수정 오류 수정 및 기능추가

--- app.py
+++ app.py
@@ -7,30 +7,55 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
 
-@app.route("/creat-member", methods=["POST"])
+# 팀원 추가
+# 숫자로 id를 주기 위해서 db에서 받아올 정보가 있기 때문에 GET을 사용한다
+# 입력받은 팀원의 정보를 추가하기 위해서 POST를 사용한다
+# methods안에는 두개를 동시에 사용할 수 있다.
+@app.route("/creat-member", methods=["GET", "POST"])
 def creat_member():
+    # 데이터베이스 안에서 members안에 문서를 카운트한다
+    # 문서가 비었으면 id를 초기화 한다
+    if (db.members.count_documents({}) == 0):
+        id = 0
+    
+    # 문서가 있다면 제일 최근 작성한 문서의 id를 받아온다.
+    else:
+        id = db.members.find().sort("_id", -1)[0]["id"]
+    
+    
+    # 입력받은 값들을 받아서 변수 저장
     image_receive = request.form["image_give"]
     name_receive = request.form["name_give"]
     yourself_receive = request.form["yourself_give"]
     strong_receive = request.form["strong_give"]
+    style_receive = request.form["style_give"]
     goals_receive = request.form["goals_give"]
     appointment_receive = request.form["appointment_give"]
     sns_receive = request.form["sns_give"]
+    # id가 겹치지 않게 1씩 더해서 id값을 증가시킨다.
+    id += 1
 
-
+    
+    # 받아온 값들을 문서로 만든다.
     doc = {
+        "id": id,
         "image": image_receive,
         "name": name_receive,
         "yourself": yourself_receive,
         "strong": strong_receive,
+        "style": style_receive,
         "goals": goals_receive,
         "appointment": appointment_receive,
         "sns": sns_receive
     }
-
+    
+    # 작성된 문서를 데이터베이스에 밀어넣는다
     db.members.insert_one(doc)
+
+    # 완료된 메세지를 보낸다
     return jsonify({"msg": "추가 완료!"})
 
 
--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// 팀원을 추가하는 폼을 닫았다 열었다 한다.
 function open_box() {
     $('#post-box').show()
 }
@@ -6,22 +7,30 @@ function close_box() {
     $('#post-box').hide()
 }
 
+//모든 HTML 문서가 준비가 되었으면 함수를 실행한다.
 $(document).ready(function () {
+    //팀원의 정보를 입력하고 버튼을 클릭하면 함수가 실행한다.
     $(".save_button").click(function () {
+        //제이쿼리를 이용해 입력한 정보의 value를 가져온다.
         const image = $("#my_image").val();
         const name = $("#my_name").val();
         const yourself = $("#my_yourself").val();
         const strong = $("#my_strong").val();
+        const style = $("#my_style").val();
         const goals = $("#my_goals").val();
         const appointment = $("#my_appointment").val();
         const sns = $("#my_sns").val();
 
+        //ajax를 통해서 서버와 연결한다.
         $.ajax({
+            //정보를 추가하기 때문에 POST를 사용한다.
+            //data를 모두 보내고 성공했으면 창을 새로고침 합니다.
             type: "POST",
             url: "/creat-member",
-            data: {image_give: image, name_give: name, yourself_give: yourself, strong_give: strong, goals_give: goals, appointment_give: appointment, sns_give: sns},
+            data: {image_give: image, name_give: name, yourself_give: yourself, strong_give: strong, style_give: style, goals_give: goals, appointment_give: appointment, sns_give: sns},
             success: function (response) {
                 alert(response["msg"]);
+                window.location.reload();
             }
         });
 
